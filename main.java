74a382e029f61563a5adceee7760874b6138ca05
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-03 23:41:32 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int u = in.nextInt();
+        System.out.println(n - u);
+    }
+}
